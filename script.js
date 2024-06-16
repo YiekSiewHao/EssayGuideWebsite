e92c27b5d213411be3070dcf5c29a5f1ea210ce8
Yiek S.H <71200758+YiekSiewHao@users.noreply.github.com>
2024-06-16 11:52:57 +0800

    Files Upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// Add any necessary JavaScript functionality here
+document.addEventListener("DOMContentLoaded", function() {
+    // Add any additional interactivity or JavaScript animations here if needed
+});
